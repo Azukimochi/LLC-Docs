68013b4dfe7dcd323b44091e4ac7e400c98652e4
Azukimochi <Azukimochi25565@gmail.com>
2023-08-25 16:21:27 +0900

    change config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,13 +6,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://azukimochi.github.io/LLC-Docs/',
+  baseUrl: '/LLC-Docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Azukimochi', // Usually your GitHub org/user name.
+  projectName: 'LLC-Docs', // Usually your repo name.
 
   presets: [
     [
